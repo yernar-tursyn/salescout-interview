57df5b25919892137e55021daa1480243d1a72c8
Yernur <yernurzhetkerbayev@gmail.com>
2024-12-20 18:55:33 +0500

    fix

--- src/__google__/update-info.ts
+++ src/__google__/update-info.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { contactInfo } from '../about-me.js';
 
-async function getGithubUserInfo(username) {
+async function getGithubUserInfo(username: string): Promise<any> {
     try {
         const response = await axios.get(`https://api.github.com/users/${username}`);
         const { name, bio, email } = response.data;
 
         const socialLinks = bio.match(/https?:\/\/[^\s]+/g) || [];
-        const tg = socialLinks.filter((link) => link.includes('t.me'));
+        const tg = socialLinks.filter((link: string | string[]) => link.includes('t.me'));
         return { name, bio, email, tg };
     } catch (error) {
         console.error('Error fetching GitHub user info:', error);
@@ -15,21 +15,21 @@ async function getGithubUserInfo(username) {
     }
 }
 
-async function updateSheet(prNumber, status, duration, userInfo) {
-    // if (status === 'failure' || userInfo.bio.length < 50) {
-        // return;
-    // } 
+async function updateSheet(prNumber: string, status: string, duration: string, userInfo: any): Promise<void> {
+    if (status === 'failure' || contactInfo.bio.length < 50) {
+        return;
+    } 
     
     try {
         await axios.post('http://164.90.212.8:3000/update-sheet', {
             prNumber: prNumber,
             status: status === 'success' ? '✅' : '❌',
             duration: `${duration} c`,
-            userInfo: {
-                name: userInfo.name || 'N/A',
-                bio: userInfo.bio || 'N/A',
-                email: userInfo.email || 'N/A',
-                phoneNumber: userInfo.phoneNumber || 'N/A',
+            contactInfo: {
+                name: contactInfo.name || 'N/A',
+                bio: contactInfo.bio || 'N/A',
+                email: contactInfo.email || 'N/A',
+                phoneNumber: contactInfo.phoneNumber || 'N/A',
             }
         })
     } catch (error){
